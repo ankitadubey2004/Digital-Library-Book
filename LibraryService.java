94c692fae81d1c3118a0aa43e76d5143c7ae4280
Ankita Dubey <155213532+ankitadubey2004@users.noreply.github.com>
2025-03-26 19:28:36 +0530

    add

--- LibraryService.java
+++ LibraryService.java
@@ -1,3 +1,11 @@
-public class LibraryService {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class LibraryService {
+    private ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(int bookId, String title, String author, String genre, String status) {
+        books.add(new Book(bookId, title, author, genre, status));
+        System.out.println("Book added successfully!");
+    }
 }
