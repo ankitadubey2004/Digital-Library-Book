709ce6d020b8657ad23cbe7b24d2ca51be30f26b
Ankita Dubey <155213532+ankitadubey2004@users.noreply.github.com>
2025-03-26 19:32:38 +0530

    commit

--- LibraryService.java
+++ LibraryService.java
@@ -8,4 +8,24 @@ public class LibraryService {
         books.add(new Book(bookId, title, author, genre, status));
         System.out.println("Book added successfully!");
     }
+
+    public void viewAllBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+            return;
+        }
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book searchBook(int bookId) {
+        for (Book book : books) {
+            if (book.getBookId() == bookId) {
+                return book;
+            }
+        }
+        return null;
+    }
+
 }
